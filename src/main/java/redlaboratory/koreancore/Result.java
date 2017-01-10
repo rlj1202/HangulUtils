05b0cd4e7ee678af8d71999457a56f39061b3cd9
RedLaboratory <rlj1202@gmail.com>
2017-01-10 13:29:51 +0900

    First commit.

---
+++ src/main/java/redlaboratory/koreancore/Result.java
@@ -0,0 +1,15 @@
+package redlaboratory.koreancore;
+
+public class Result {
+	
+	public String STR;
+	public int CURSOR;
+	public int SELECTION;
+	
+	public Result(String str, int cursorPos, int selecPos) {
+		STR = str;
+		CURSOR = cursorPos;
+		SELECTION = selecPos;
+	}
+	
+}
